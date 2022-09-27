93f663b391f0b5813bceb99a3582184500119aec
“thamiem-n” <thamiem123@hotmail.com>
2022-09-27 17:57:58 +0100

    File restructuring and npm dependencies

---
+++ server/data.js
@@ -0,0 +1,35 @@
+
+const anime =  [
+    {   
+        name: 'Attack on Titan', 
+        description: 'It is set in a world where humanity is forced to live in cities surrounded by three enormous walls that protect them from gigantic man-eating humanoids referred to as Titans '
+        
+    },
+    {   
+        name: 'Attack on Titan', 
+        description: 'It is set in a world where humanity is forced to live in cities surrounded by three enormous walls that protect them from gigantic man-eating humanoids referred to as Titans '
+        
+    },
+    {   
+        name: 'Attack on Titan', 
+        description: 'It is set in a world where humanity is forced to live in cities surrounded by three enormous walls that protect them from gigantic man-eating humanoids referred to as Titans '
+        
+    },
+    {   
+        name: 'Attack on Titan', 
+        description: 'It is set in a world where humanity is forced to live in cities surrounded by three enormous walls that protect them from gigantic man-eating humanoids referred to as Titans '
+        
+    },
+    {   
+        name: 'Attack on Titan', 
+        description: 'It is set in a world where humanity is forced to live in cities surrounded by three enormous walls that protect them from gigantic man-eating humanoids referred to as Titans '
+        
+    },
+
+
+
+
+
+
+
+]
