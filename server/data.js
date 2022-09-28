9fd1ec35eb8217656ef61e3c19ba616cd78166a7
“thamiem-n” <thamiem123@hotmail.com>
2022-09-28 12:10:37 +0100

    data array complete

--- server/data.js
+++ server/data.js
@@ -21,40 +21,45 @@ const gameArray =  [
     {   
         id: 4,
         name: "Modern Warfare 2", 
-        genre: 'first-person shooter',
+        genre: "first-person shooter",
         description: "Call of Duty: Modern Warfare 2 is a 2009 first-person shooter game developed by Infinity Ward and published by Activision. It is the sixth installment in the Call of Duty series and the direct sequel to Call of Duty 4: Modern Warfare."
     },
     {   
         id: 5,
-        name: 'Assasins Creed: Black Flag', 
+        name: "Assasins Creed: Black Flag", 
         genre: "action-adventure",
         description: "The plot is set in a fictional history of real-world events and follows the millennia-old struggle between the Assassins, who fight to preserve peace and free will, and the Templars, who desire peace through control."
     },
 
     {   
         id: 6,
-        name: '', 
-        description: ''
+        name: "The Witcher 3: Wild Hunt",
+        genre: "action role-playing",
+        description: "The game takes place in a fictional fantasy world based on Slavic mythology. Players control Geralt of Rivia, a monster slayer for hire known as a Witcher, and search for his adopted daughter, who is on the run from the otherworldly Wild Hunt. Players battle the game's many dangers with weapons and magic, interact with non-player characters, and complete quests to acquire experience points and gold, which are used to increase Geralt's abilities and purchase equipment."
     },
     {   
         id: 7,
-        name: 'GTA 5', 
-        description: ''
+        name: "GTA 5", 
+        genre: "action-adventure",
+        description: "The game is played from either a third-person or first-person perspective, and its world is navigated on foot and by vehicle. Players control the three lead protagonists throughout single-player and switch among them, both during and outside missions. The story is centred on the heist sequences, and many missions involve shooting and driving gameplay."
     },
     {   
         id: 8,
-        name: 'Mario', 
-        description: ''
+        name: "New Super Mario Bros.", 
+        genre: "platform",
+        description: "A 2D side-scrolling platform video game developed and published in 2012 by Nintendo as a launch game for the Wii U. It is the fourth entry in the New Super Mario Bros. series. "
     },
     {   
         id: 9,
-        name: 'Ghosts of Tsushima ', 
-        description: ''
+        name: "Ghosts of Tsushima", 
+        genre: "action-adventure",
+        description: "A action-adventure game developed by Sucker Punch Productions and published by Sony Interactive Entertainment. The player controls Jin Sakai, a samurai on a quest to protect Tsushima Island during the first Mongol invasion of Japan. Jin must choose between his warrior code to fight honorably, and pragmatic but despicable ways that can repel the Mongols with minimal casualties."
     },
     {   
         id: 10,
-        name: 'Dark Souls', 
-        description: ''
+        name: "Elden Ring", 
+        genre: "action role-playing",
+        description: "In Elden Ring, players control a customizable player character on a journey to repair the Elden Ring and become the new Elden Lord. The game is presented through a third-person perspective, with players freely roaming its interactive open world. Gameplay elements include combat using several types of weapons and magic spells, horseback riding, and crafting."
     }
 ]
 
