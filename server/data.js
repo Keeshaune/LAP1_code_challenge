82b2161c97e910783e4b076d5018049d1d692019
“thamiem-n” <thamiem123@hotmail.com>
2022-09-28 11:52:30 +0100

    updated made to data.js

--- server/data.js
+++ server/data.js
@@ -2,29 +2,33 @@
 const gameArray =  [
     {   
         id: 1,
-        name: 'Uncharted 4', 
-        description: ' '
+        name: "Uncharted 4",
+        genre: "action-adventure", 
+        description: "Set several years after the events of Uncharted 3: Drake's Deception, players control Nathan Drake, a former treasure hunter coaxed out of retirement by his presumed-dead brother Samuel. With Nathan's longtime partner, Victor Sullivan, they search for clues for the location of Henry Avery's long-lost treasure."
     },
     {   
         id: 2,
-        name: 'World of Warcraft', 
-        description: ' ' 
+        name: "World of Warcraft", 
+        genre: "mmorpg",
+        description: "Set in the fictional world of Azeroth, WoW allows players to create avatar-style characters and explore a sprawling universe while interacting with nonreal players—called nonplayer characters (NPCs)—and other real-world players (PCs)." 
     },
     {   
         id: 3,
-        name: 'God of War', 
-        description: ''
+        name: "God of War", 
+        genre: 'action-adventure',
+        description: "Following the death of Kratos' second wife, Atreus' mother, they journey to fulfill her request that her ashes be spread at the highest peak of the nine realms. Kratos keeps his troubled past a secret from Atreus, who is unaware of his divine nature. Along their journey, they come into conflict with monsters and gods of the Norse world."
     },
     {   
         id: 4,
-        name: 'Modern Warfare 2', 
-        description: ''
-        
+        name: "Modern Warfare 2", 
+        genre: 'first-person shooter',
+        description: "Call of Duty: Modern Warfare 2 is a 2009 first-person shooter game developed by Infinity Ward and published by Activision. It is the sixth installment in the Call of Duty series and the direct sequel to Call of Duty 4: Modern Warfare."
     },
     {   
         id: 5,
         name: 'Assasins Creed: Black Flag', 
-        description: ''
+        genre: "action-adventure",
+        description: "The plot is set in a fictional history of real-world events and follows the millennia-old struggle between the Assassins, who fight to preserve peace and free will, and the Templars, who desire peace through control."
     },
 
     {   
