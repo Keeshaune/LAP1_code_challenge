2e6fdd7abf600d0d2fdea709bafb8e95bc86e9ee
Sean <millington.sean12@gmail.com>
2022-09-28 16:39:45 +0200

    functioning cards

--- client/index.js
+++ client/index.js
@@ -3,51 +3,46 @@ const luckyButton = document.getElementById('lucky')
 const searchBar = document.getElementById('input')
 const resultList = document.getElementById('results')
 
-
-
 const main = document.querySelector('.main')
-const back = document.getElementById('bkbutton')
-
 
 searchButton.addEventListener('click', handleSearch)
-luckyButton.addEventListener('click', test)
+luckyButton.addEventListener('click', handleLucky)
 
 async function handleSearch(e){
     e.preventDefault();
+    clearList()
     const gameData = await fetch(`http://localhost:3000/games`)
     const gameDataJson = await gameData.json();
-    // main.innerHTML = ""
-    // const backButton =  document.createElement('button');
-    // backButton.textContent = "back"
-    // back.appendChild(backButton)
-    // let li;
-    // gameDataJson.forEach(element => {
-    //     li = document.createElement('li')
-    //     li.textContent = element.name;
-    //     resultList.appendChild(li)
-    // });
-    let div;
     gameDataJson.forEach(element => {
-        div = document.createElement('div')
-        div.innerHTML = `
-            <ul>
-                <li>${element.name}</li>
-                <li>${element.genre}</li>
-                <li>${element.description}</li>
-            </ul>
-        `
-        resultList.appendChild(div)
+        createCard(element)
     });
 }
 
 async function handleLucky(e) {
     e.preventDefault();
-
+    clearList()
     const randomData = await fetch(`http://localhost:3000/games/lucky`)
-    const randomDataJson = await randomData.json();
-    main.innerHTML = "";
+    randomDataJson = await randomData.json();
+    createCard(randomDataJson)
 }
 
 function test () {
     console.log('hello')
+}
+
+function clearList() {
+    resultList.innerHTML = ""
+}
+
+function createCard(input) {
+    resultList.innerHTML += `
+        <div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><strong>name:</strong> ${input.name}</li>
+                <li class="list-group-item"><strong>genre:</strong> ${input.genre}</li>
+                <li class="list-group-item"><strong>description:</strong> ${input.description}</li>
+            </ul>
+        </div>
+        <br>
+        `
 }
