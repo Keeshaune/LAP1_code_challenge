332f4512ee0a8d987282f396bedc095918af3ae7
Sean <millington.sean12@gmail.com>
2022-09-28 15:21:23 +0200

    trying to add results list

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ const searchButton = document.getElementById('search')
 const luckyButton = document.getElementById('lucky')
 const searchBar = document.getElementById('input')
 const resultList = document.getElementById('results')
+const main = document.querySelector('.main')
+const back = document.getElementById('bkbutton')
 
 searchButton.addEventListener('click', handleSearch)
 luckyButton.addEventListener('click', test)
@@ -10,7 +12,11 @@ async function handleSearch(e){
     e.preventDefault();
     const gameData = await fetch(`http://localhost:3000/games`)
     const gameDataJson = await gameData.json();
-    console.log(gameDataJson)
+    console.log(gameDataJson[0])
+    // main.innerHTML = ""
+    const backButton =  document.createElement('button');
+    backButton.textContent = "back"
+    back.appendChild(backButton)
     let li;
     gameDataJson.forEach(element => {
         li = document.createElement('li')
