27c6ab24715c9eb9a5ea676e5cf3e9568ac34e4b
Sean <millington.sean12@gmail.com>
2022-09-30 10:48:55 +0200

    final changes, cleaning up code

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const cors = require('cors');
 const app = express()
+const gameArray = require('./data');
 
 const port = 3000;
 app.use(cors());
-
-const gameArray = require('./data');
 
 app.get('/', (req, res) => {
   res.status(200).send('hello');
@@ -18,16 +17,16 @@ app.get('/games', (req, res) => {
 app.get('/games/lucky', (req, res) => {
   let randomGame = gameArray[Math.floor(Math.random()*gameArray.length)];
   res.status(200).send(randomGame);
- })
+})
 
- app.get('/games/:name', (req, res) => {
+app.get('/games/:name', (req, res) => {
   const name = req.params.name.toLowerCase()
   const game = gameArray.filter(ele => ele.name.toLowerCase().includes(name));
   res.status(200).send(game);
- })
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  console.log(`Example app listening on port ${port}`)
+})
 
-  module.exports = app;
+module.exports = app;
--- server/test/app.test.js
+++ server/test/app.test.js
@@ -18,26 +18,11 @@ describe("google API",  ()=>{
 		api.close(done);
 	});
 
-    it("can it work?", (done) => {
+    it("sends back a status of 200", (done) => {
 		request(api).get("/").expect(200,done);
 	});
 
-    it("can it work?", (done) => {
+    it("fetches the full list of games", (done) => {
 		request(api).get("/games").expect(200).expect(gameArray,done);
 	});
-
-    // it("can it work?", async () => {
-    //     const data = await request(api).get("/games/lucky")
-	// 	.expect(200).expect(typeof data).toBe("object");
-	// });
-
-    
 });
-
-
-
-  
-//   app.get('/games/lucky', (req, res) => {
-//     let randomGame = gameArray[Math.floor(Math.random()*gameArray.length)];
-//     res.send(randomGame);
-//    })
